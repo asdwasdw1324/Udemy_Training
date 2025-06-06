4abef0cec2ccae632c5afc65059deaacb9b80100
Frank、Yang <278586884@qq.com>
2025-06-06 15:30:57 +0800

    Update WarriorStructTypes.h

--- Source/Udemy_Training/Public/WarriorStructTypes.h
+++ Source/Udemy_Training/Public/WarriorStructTypes.h
@@ -29,12 +29,15 @@ struct FWarriorHeroWeaponData
 {
 	GENERATED_BODY()
 
+	//The anim layer will link to the hero character which it will perform when equipping this weapon.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TSubclassOf<UWarriorHeroLinkedAnimLayer> WeaponAnimLayerToLink;
 
+	//The input mapping context that will be used when the weapon is equipped.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
 	TObjectPtr<UInputMappingContext> WeaponInputMappingContext;
-	
+
+	//The abilities that will be granted to the hero character when the weapon is equipped.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
 	TArray<FWarriorHeroAbilitySet> DefaultWeaponAbilities;
 };
