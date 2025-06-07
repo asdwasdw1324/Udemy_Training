834ea477b4e4d1e41aeb10dfeb2f91dd54768456
Frank、Yang <278586884@qq.com>
2025-06-07 14:44:19 +0800

    update library

--- Source/Udemy_Training/Private/BaseGameTags.cpp
+++ Source/Udemy_Training/Private/BaseGameTags.cpp
@@ -17,6 +17,9 @@ namespace WarriorGameplayTags
 	/** player Tags**/
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
 
+	/** Player States**/
+	UE_DEFINE_GAMEPLAY_TAG(Player_Status_JumpToFinisher, "Player.Status.JumpToFinisher");
+	
 	/** Ability Tags**/
 	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe, "Player.Event.Equip.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Axe, "Player.Ability.Equip.Axe");
