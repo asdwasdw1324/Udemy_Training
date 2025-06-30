99571b7ac2ff4bc88e4d31507cdf8f69769109f7
Frank、Yang <51279940+asdwasdw1324@users.noreply.github.com>
2025-07-01 01:53:44 +0800

    2025/7/1_1

--- Source/Udemy_Training/Private/GameplayAbility/GEExecutionCalc_DamageTaken.cpp
+++ Source/Udemy_Training/Private/GameplayAbility/GEExecutionCalc_DamageTaken.cpp
@@ -2,4 +2,88 @@
 
 
 #include "GameplayAbility/GEExecutionCalc_DamageTaken.h"
+#include "BaseAttributeSet.h"
+#include "BaseGameTags.h"
+#include "../../../../../../../../../Program Files/Epic Games/UE_5.5/Engine/Plugins/Runtime/USDCore/Source/ThirdParty/USD/include/pxr/base/gf/api.h"
 
+struct FWarriorDamageCapture 
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(AttackPower);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(DefensePower);
+
+	FWarriorDamageCapture()
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, AttackPower, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UBaseAttributeSet, DefensePower, Target, false);
+	}
+};
+
+static const FWarriorDamageCapture& GetWarriorDamageCapture()
+{
+	static FWarriorDamageCapture DamageCapture;
+	return DamageCapture;
+}
+
+UGEExecutionCalc_DamageTaken::UGEExecutionCalc_DamageTaken()
+{
+
+
+	/**
+	 * 捕获AttackPower属性(Slow way)
+	FProperty* AttackPowerProperty = FindFieldChecked<FProperty>(UBaseAttributeSet::StaticClass(), GET_MEMBER_NAME_CHECKED(UBaseAttributeSet, AttackPower));
+
+	FGameplayEffectAttributeCaptureDefinition AttackPowerCaptureDef(AttackPowerProperty, EGameplayEffectAttributeCaptureSource::Source, false);
+
+	RelevantAttributesToCapture.Add(AttackPowerCaptureDef);
+	*/
+
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().AttackPowerDef);
+	RelevantAttributesToCapture.Add(GetWarriorDamageCapture().DefensePowerDef);
+}
+
+void UGEExecutionCalc_DamageTaken::Execute_Implementation(
+	const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	//Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+	const FGameplayEffectSpec& EffectSpec = ExecutionParams.GetOwningSpec();
+
+	// EffectSpec.GetContext().GetSourceObject();
+	// EffectSpec.GetContext().GetAbility();
+	// EffectSpec.GetContext().GetEffectCauser();
+	// EffectSpec.GetContext().GetInstigator();
+	
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = EffectSpec.CapturedSourceTags.GetAggregatedTags();
+	EvaluationParameters.TargetTags = EffectSpec.CapturedTargetTags.GetAggregatedTags();
+
+	float SourceAttackPower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().AttackPowerDef, EvaluationParameters, SourceAttackPower);
+
+	float BaseDamage = 0.f;
+	int32 UsedLightAttackComboCount = 0;
+	int32 UsedHeavyAttackComboCount = 0;
+
+	for (const TPair<FGameplayTag, float>& TagMagnitude : EffectSpec.SetByCallerTagMagnitudes)
+	{
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Shared_SetByCaller_BaseDamage))
+		{
+			BaseDamage = TagMagnitude.Value;
+		}
+
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Light))
+		{
+			UsedLightAttackComboCount = TagMagnitude.Value;
+		}
+
+		if (TagMagnitude.Key.MatchesTagExact(WarriorGameplayTags::Player_SetByCaller_AttackType_Heavy))
+		{
+			UsedHeavyAttackComboCount = TagMagnitude.Value;
+		}
+	}
+
+	float SourceDefensePower = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetWarriorDamageCapture().DefensePowerDef, EvaluationParameters, SourceDefensePower);
+	
+}
--- Source/Udemy_Training/Public/GameplayAbility/GEExecutionCalc_DamageTaken.h
+++ Source/Udemy_Training/Public/GameplayAbility/GEExecutionCalc_DamageTaken.h
@@ -13,5 +13,9 @@ UCLASS()
 class UDEMY_TRAINING_API UGEExecutionCalc_DamageTaken : public UGameplayEffectExecutionCalculation
 {
 	GENERATED_BODY()
-	
+
+public:
+	UGEExecutionCalc_DamageTaken();
+
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, OUT FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
 };
